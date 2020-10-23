d1d1e7ee452697d660986f2ed1f58b925e04b1fe
Leontheking24 <LeonAsylum@web.de>
2020-10-23 13:15:17 +0200

    Updated Sql-Config

--- src/main/java/de/leontheking24/discordbot/Database/Config/DatabaseGlobalConfig.java
+++ src/main/java/de/leontheking24/discordbot/Database/Config/DatabaseGlobalConfig.java
@@ -6,7 +6,7 @@ public class DatabaseGlobalConfig extends DatabaseConfig {
     private final static String server = "45.142.178.20";
     private final static int port = 3306;
     private final static String databaseName = "Discord";
-    private final static String username = "Leon";
+    private final static String username = "root";
     private final static String password = "WfePGfsvmSuwams24";
 
     public DatabaseGlobalConfig() {
